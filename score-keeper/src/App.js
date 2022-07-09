ab30a161f41129c42a647302a4bbd7cf84f83069
Sandra Margol <sandramargol@Sandras-MacBook-Air.local>
2022-07-09 09:36:48 +0100

    logic added to ScoreKeeper component

--- score-keeper/src/App.js
+++ score-keeper/src/App.js
@@ -1,12 +1,15 @@
-
 import ScoreKeeper from './ScoreKeeper';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
+      <div>
       <ScoreKeeper/>
+    </div>
     </div>
   );
 }
 
 export default App;
+
--- score-keeper/src/ScoreKeeper.js
+++ score-keeper/src/ScoreKeeper.js
@@ -1,29 +1,108 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+
 const ScoreKeeper = () => {
 
-    
+  const [winningScore, setWinningScore] = useState(0)
+  const [score, setScore] = useState(0)
+  const [scoreTwo, setScoreTwo] = useState(0)
+  const [isGameOver, setGameOver] = useState(false)
+  const [colorOne, setColorOne] = useState('')
+  const [colorTwo, setColorTwo] = useState('')
 
-    return ( <div className="content">
-    <div className="image">
-    <img src="https://images.unsplash.com/photo-1584813539806-2538b8d918c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" />
-    </div>
-    <h1>Pink Pong Score Keeper</h1>
-    <h1><span className="displayOne">0</span>to
-    <span className="displayTwo">0</span></h1>
-    <select>
-      <option>3</option>
-      <option>4</option>
-      <option>5</option>
-      <option>6</option>
-      <option>7</option>
-      <option>8</option>
-      <option>9</option>
-      <option>10</option>
-      <option>11</option>
-    </select>
-    <button className="playerOne">Player One</button>
-    <button className="playerTwo">Player Two</button>
-    <button className="reset">Reset</button>
-  </div> );
+
+  const selectRound = (e) => {
+    const playingTo = parseInt(e.target.value)
+    setWinningScore(playingTo)
+    setGameOver(false)
+    setScore(0) || setScoreTwo(0)
+    setColorOne('')
+    setColorTwo('')
+  }
+
+  const playerOneHandler = (e) => {
+    if (!isGameOver) {
+      if (score !== winningScore) {
+        setScore(score + 1)
+      } else {
+        setGameOver(true)
+      }
+    }
+  }
+
+  const playerTwoHandler = () => {
+    if (!isGameOver) {
+      if (scoreTwo !== winningScore) {
+        setScoreTwo(scoreTwo + 1)
+      } else {
+        setGameOver(true)
+      }
+    }
+  }
+  useEffect(() => {
+      if(isGameOver)
+      if(score === winningScore) {
+        setColorOne('green')
+        setColorTwo('red')
+      } else {
+        if (scoreTwo === winningScore) {
+          setColorTwo('green')
+          setColorOne('red')
+        }
+      }
+  }, [score, scoreTwo, winningScore, isGameOver])
+
+  const resetHandler = () => {
+    setGameOver(false)
+    setScore(0) || setScoreTwo(0)
+    setColorOne('')
+    setColorTwo('')
+  }
+
+  return (<div className="content">
+    <div className="text-center">
+      <div className="row d-flex justify-content-center">
+        <div className="card" style={{ "width": "45rem" }}>
+          <img src="https://images.unsplash.com/photo-1584813539806-2538b8d918c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="" />
+          <div className="card-body">
+            <h5 className="card-title">Ping Pong Score Keeper</h5>
+          </div>
+          <h1><span className={colorOne}>{score}</span> to <span className={colorTwo}>{scoreTwo}</span></h1>
+          <h6>Use the buttons below to keep score</h6>
+          <div>
+            <h6 className="selectScore">Playing to:</h6>
+            <select value={winningScore} onChange={selectRound}>
+              <option>3</option>
+              <option>4</option>
+              <option>5</option>
+              <option>6</option>
+              <option>7</option>
+              <option>8</option>
+              <option>9</option>
+              <option>11</option>
+              <option>12</option>
+              <option>13</option>
+              <option>14</option>
+              <option>15</option>
+            </select>
+          </div>
+          <div className="card-body">
+            <button onClick={playerOneHandler} type="button" className="playerOne btn btn-primary">Player One</button>
+            <button onClick={playerTwoHandler} type="button" className="playerTwo btn btn-success">Player Two</button>
+            <button onClick={resetHandler} type="button" className="reset btn btn-danger">Reset</button>
+          </div>
+        </div>
+      </div>
+    </div>);
+  </div>);
 }
- 
+
 export default ScoreKeeper;
+
+
+
+
+
+
